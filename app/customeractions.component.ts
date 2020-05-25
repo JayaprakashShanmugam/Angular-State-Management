390aadcee90f8f95529594dc323aee3792e82909
= <=>
2020-05-25 10:46:37 +0530

    Angular state app

---
+++ app/customeractions.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customeractions',
+  template: `
+    <p>
+      customeractions works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CustomeractionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
